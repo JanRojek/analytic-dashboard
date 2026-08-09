3119e3faff1e47bbc7c353e89ab81bc945bf102c
JanRojek <jan.rojek@studenci.collegiumwitelona.pl>
2026-08-09 19:52:22 +0200

    add integration test foundation

---
+++ backend/tests/AnalyticDashboard.IntegrationTests/ApiSmokeTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AnalyticDashboard.IntegrationTests;
+
+public class ApiSmokeTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public ApiSmokeTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public void Factory_ShouldCreateClient()
+    {
+        using var client = _factory.CreateClient();
+
+        Assert.NotNull(client);
+    }
+}
